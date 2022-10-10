7bb9a9ee298f950c00f8d4db08489d4456d9c9d7
AkshitaRahangdale <akshitarahangdale@1720gmail.com>
2022-10-10 16:27:56 +0530

    Add project files.

---
+++ TestProject1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    public class Product
+    {
+        private int id, price;
+        private string name;
+        public Product(int id, int price, string name)
+        {
+            this.id = id;
+            this.price = price;
+            this.name = name;
+        }
+        public string Print()
+        {
+            return $"{id}{name}{price}";
+        }
+
+        static void Main(string[] args)
+        {
+            Product p1 = new Product(123, 56, "key");
+            Product p2 = new Product(1236, 46, "ray");
+            Console.WriteLine(p1.Print());
+            Console.WriteLine(p2.Print());
+        }
+    }
+}
